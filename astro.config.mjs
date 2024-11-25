34309dd77e430b7717650dbd8e3871fdd01aeab4
Alice <138005616+YashajinAlice@users.noreply.github.com>
2024-11-25 23:55:33 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://YashajinAlice.github.io',
+  site: 'http://fulin-net.top/',
   base: '/FuLinA.github.io/',
 
   trailingSlash: 'always',
