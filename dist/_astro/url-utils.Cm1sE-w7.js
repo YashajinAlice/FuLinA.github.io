c91ba63e8127d48d12a63f7a4483936fb5ddd1e7
Alice <138005616+YashajinAlice@users.noreply.github.com>
2024-12-22 19:15:53 +0800

    up

---
+++ dist/_astro/url-utils.Cm1sE-w7.js
@@ -0,0 +1,1 @@
+import"./zh_TW.CcEX6emu.js";function i(e,r){return e.replace(/^\/|\/$/g,"").toLowerCase()===r.replace(/^\/|\/$/g,"").toLowerCase()}function t(...e){return e.join("/").replace(/\/+/g,"/")}function u(e){return t("","/FuLinA.github.io/",e)}export{i as p,u};
---
+++ src/config.ts
@@ -0,0 +1,78 @@
+import type {
+  LicenseConfig,
+  NavBarConfig,
+  ProfileConfig,
+  SiteConfig,
+} from './types/config'
+import { LinkPreset } from './types/config'
+
+export const siteConfig: SiteConfig = {
+  title: 'FuLin-net',
+  subtitle: 'FrrEE_fulin-net',
+  lang: 'zh_TW',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
+  themeColor: {
+    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: false,     // Hide the theme color picker for visitors
+  },
+  banner: {
+    enable: true,
+    src: 'assets/images/banner.jpg',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+    credit: {
+      enable: false,         // Display the credit text of the banner image
+      text: '',              // Credit text to be displayed
+      url: ''                // (Optional) URL link to the original artwork or artist's page
+    }
+  },
+  favicon: [    // Leave this array empty to use the default favicon
+    {
+      src: '/favicon/dev-icon.png',    // icon 的路徑，相對於 /public 目錄
+      theme: 'light',              // (可選) 'light' 或 'dark'，如果您有不同主題的 icon，請設置此項
+      sizes: '32x32',              // (可選) icon 的尺寸，如果您有不同尺寸的 icon，請設置此項
+    },
+  ]
+}
+
+export const navBarConfig: NavBarConfig = {
+  links: [
+    LinkPreset.Home,
+    LinkPreset.Archive,
+    LinkPreset.About,
+    {
+      name: 'GitHub',
+      url: 'https://github.com/YashajinAlice',     // Internal links should not include the base path, as it is automatically added
+      external: true,                               // Show an external link icon and will open in a new tab
+    },
+  ],
+}
+
+export const profileConfig: ProfileConfig = {
+  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: '芙檁',
+  bio: 'Discord機器人開發者',
+  links: [
+    {
+      name: 'Gmail',
+      icon: 'fa6-brands:google',       // Visit https://icones.js.org/ for icon codes
+                                        // You will need to install the corresponding icon set if it's not already included
+                                        // `pnpm add @iconify-json/<icon-set-name>`
+      url: 'overcar000@gmail.com',
+    },
+    {
+      name: 'Discord',
+      icon: 'fa6-brands:discord',
+      url: 'https://discord.gg/Ntat5HRFqT',
+    },
+    {
+      name: 'GitHub',
+      icon: 'fa6-brands:github',
+      url: 'https://github.com/YashajinAlice',
+    },
+  ],
+}
+
+export const licenseConfig: LicenseConfig = {
+  enable: true,
+  name: 'CC BY-NC-SA 4.0',
+  url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+}
